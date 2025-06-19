a8160c658e0cd7bcfd525a1fff6bbbc1f6c1d89e
kdowd <kevin.dowd@gmail.com>
2025-06-19 12:29:28 +1200

    update 1

--- main.js
+++ main.js
@@ -1,35 +1,23 @@
-
-
-
-document.body.addEventListener("DOMContentLoaded", onStart);
-
-
+document.body.addEventListener('DOMContentLoaded', onStart);
 
 function openPage(pageName, elmnt, color) {
+	let i, tabcontent, tablinks;
+	tabcontent = document.querySelectorAll('.tabcontent');
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = 'none';
+	}
 
-    let i, tabcontent, tablinks;
-    tabcontent = document.querySelectorAll(".tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+	tablinks = document.querySelectorAll('.tablink');
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].style.backgroundColor = '';
+	}
 
+	document.getElementById(pageName).style.display = 'block';
 
-    tablinks = document.querySelectorAll(".tablink");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.backgroundColor = "";
-    }
-
-
-    document.getElementById(pageName).style.display = "block";
-
-
-    elmnt.style.backgroundColor = color;
+	elmnt.style.backgroundColor = color;
 }
 
 function onStart() {
-    document.querySelector("#defaultOpen").click();
+	document.querySelector('#defaultOpen').click();
+	document.querySelector('.tablink').click();
 }
-
-
-
-
